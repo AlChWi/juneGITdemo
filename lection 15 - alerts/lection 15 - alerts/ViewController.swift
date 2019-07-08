51ecf9b9d652f301fd291d1753341a66baa4ca4a
Oleksii Perov <oleksii.perov@nure.ua>
2019-07-08 20:41:07 +0300

    created project

---
+++ lection 15 - alerts/lection 15 - alerts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lection 15 - alerts
+//
+//  Created by Alex P on 08/07/2019.
+//  Copyright © 2019 Alex Perov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
